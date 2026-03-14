c4b4dd2d604e9552c1bc7b054535cc48af529c83
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-03-14 17:41:12 +1100

    CORS

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -1,5 +1,7 @@
 export function sendResponse(res, header, status, payload) {
   res.setHeader('Content-Type', header);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Method', 'GET');
   res.statusCode = status;
   res.end(JSON.stringify(payload));
 }
