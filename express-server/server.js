c98befc9098bbf68d4962bcabead803d63abf2c4
Thuy Katie Nguyen <katiefirst7@gmail.com>
2026-03-19 23:25:51 +1100

    a simpliest form of express server

---
+++ express-server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { apiRouter } from './routes/apiRoutes';
+const PORT = 8000;
+
+const app = express();
+app.use('/api', apiRouter);
+//if route unfound
+app.use((req, res) => {
+  res.status('404').json({ message: 'Endpoint not found' });
+});
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`));
